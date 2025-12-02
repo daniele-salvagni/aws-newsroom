aa777c129bdbd4ff51c034b17ce69a545fc20dc3
Daniele Salvagni <daniele.salvagni@gmail.com>
2025-12-03 00:04:53 +0100

    feat: events (handled manually)

---
+++ database/events.sql
@@ -0,0 +1,8 @@
+-- Events (managed manually for now)
+INSERT INTO events (event_id, title, url, category, start_time, end_time)
+VALUES
+    ('reinvent-2025-garman', 'AWS re:Invent opening keynote with Matt Garman, AWS CEO', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-02 08:00:00-08', '2025-12-02 10:30:00-08'),
+    ('reinvent-2025-sivasubramanian', 'The Future of Agentic AI is Here', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-03 08:00:00-08', '2025-12-03 10:30:00-08'),
+    ('reinvent-2025-borno', 'The Future of Agentic AI is Here', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-03 15:00:00-08', '2025-12-03 16:30:00-08'),
+    ('reinvent-2025-desantis-brown', 'Infrastructure Innovations', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-04 08:00:00-08', '2025-12-04 10:30:00-08'),
+    ('reinvent-2025-vogels', 'A Special Closing Keynote with Dr. Werner Vogels', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-04 15:00:00-08', '2025-12-04 16:30:00-08');
