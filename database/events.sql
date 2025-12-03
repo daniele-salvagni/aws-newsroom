416049c2eeb8e8a8490c9fc10555d3b8291c4a80
Daniele Salvagni <daniele.salvagni@gmail.com>
2025-12-03 16:59:39 +0100

    fix: db column is not tz aware

--- database/events.sql
+++ database/events.sql
@@ -1,8 +1,23 @@
 -- Events (managed manually for now)
 INSERT INTO events (event_id, title, url, category, start_time, end_time)
 VALUES
-    ('reinvent-2025-garman', 'AWS re:Invent opening keynote with Matt Garman, AWS CEO', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-02 08:00:00-08', '2025-12-02 10:30:00-08'),
-    ('reinvent-2025-sivasubramanian', 'The Future of Agentic AI is Here', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-03 08:00:00-08', '2025-12-03 10:30:00-08'),
-    ('reinvent-2025-borno', 'The Future of Agentic AI is Here', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-03 15:00:00-08', '2025-12-03 16:30:00-08'),
-    ('reinvent-2025-desantis-brown', 'Infrastructure Innovations', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-04 08:00:00-08', '2025-12-04 10:30:00-08'),
-    ('reinvent-2025-vogels', 'A Special Closing Keynote with Dr. Werner Vogels', 'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025', '2025-12-04 15:00:00-08', '2025-12-04 16:30:00-08');
+    ('reinvent-2025-garman',
+     'AWS re:Invent opening keynote with Matt Garman, AWS CEO',
+     'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025',
+     '2025-12-02 16:00:00', '2025-12-02 18:30:00'),
+    ('reinvent-2025-sivasubramanian',
+     'The Future of Agentic AI is Here',
+     'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025',
+     '2025-12-03 16:00:00', '2025-12-03 18:30:00'),
+    ('reinvent-2025-borno',
+     'The Future of Agentic AI is Here',
+     'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025',
+     '2025-12-03 23:00:00', '2025-12-04 00:30:00'),
+    ('reinvent-2025-desantis-brown',
+     'Infrastructure Innovations',
+     'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025',
+     '2025-12-04 16:00:00', '2025-12-04 18:30:00'),
+    ('reinvent-2025-vogels',
+     'A Special Closing Keynote with Dr. Werner Vogels',
+     'https://reinvent.awsevents.com/keynotes/', 're:Invent 2025',
+     '2025-12-04 23:00:00', '2025-12-05 00:30:00');
